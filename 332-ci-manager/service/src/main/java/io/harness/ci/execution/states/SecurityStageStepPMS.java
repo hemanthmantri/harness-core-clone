4b11f9be64dfe4810ddd67d816090829cdf9a177
Sergey Bobrov <sergey.bobrov@harness.io>
2023-12-28 16:54:23 +0000

    feat: [STO-6837]: persist step parameters (#146)

--- 332-ci-manager/service/src/main/java/io/harness/ci/execution/states/SecurityStageStepPMS.java
+++ 332-ci-manager/service/src/main/java/io/harness/ci/execution/states/SecurityStageStepPMS.java
@@ -13,6 +13,7 @@ import static io.harness.steps.SdkCoreStepUtils.createStepResponseFromChildRespo
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.app.beans.entities.StepExecutionParameters;
 import io.harness.beans.stages.IntegrationStageStepParametersPMS;
 import io.harness.beans.steps.outcome.CIStepArtifactOutcome;
 import io.harness.beans.steps.outcome.IntegrationStageOutcome;
@@ -40,6 +41,8 @@ import io.harness.pms.sdk.core.steps.io.StepInputPackage;
 import io.harness.pms.sdk.core.steps.io.StepResponse;
 import io.harness.pms.sdk.core.steps.io.StepResponse.StepResponseBuilder;
 import io.harness.pms.sdk.core.steps.io.StepResponseNotifyData;
+import io.harness.pms.serializer.recaster.RecastOrchestrationUtils;
+import io.harness.repositories.StepExecutionParametersRepository;
 import io.harness.tasks.ResponseData;
 
 import com.google.inject.Inject;
@@ -53,6 +56,8 @@ import lombok.extern.slf4j.Slf4j;
 public class SecurityStageStepPMS implements ChildExecutable<StageElementParameters> {
   public static final StepType STEP_TYPE =
       StepType.newBuilder().setType("SecurityStageStepPMS").setStepCategory(StepCategory.STAGE).build();
+
+  @Inject private StepExecutionParametersRepository stepExecutionParametersRepository;
 
   @Inject ExecutionSweepingOutputService executionSweepingOutputResolver;
   @Inject OutcomeService outcomeService;
@@ -70,6 +75,15 @@ public class SecurityStageStepPMS implements ChildExecutable<StageElementParamet
 
     log.info("Executing integration stage with params accountId {} projectId {} [{}]", accountId, projectIdentifier,
         stepParameters);
+
+    String stageRuntimeId = AmbianceUtils.getStageRuntimeIdAmbiance(ambiance);
+
+    stepExecutionParametersRepository.save(StepExecutionParameters.builder()
+            .accountId(AmbianceUtils.getAccountId(ambiance))
+            .stageRunTimeId(stageRuntimeId)
+            .runTimeId(AmbianceUtils.obtainCurrentRuntimeId(ambiance))
+            .stepParameters(RecastOrchestrationUtils.toJson(stepParameters))
+            .build());
 
     IntegrationStageStepParametersPMS integrationStageStepParametersPMS =
         (IntegrationStageStepParametersPMS) stepParameters.getSpecConfig();
